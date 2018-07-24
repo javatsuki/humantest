8fbf2e457cfe948ffc2d38aab9203ae425b043b9
wakatsuki <wakatsuki@PC-0120>
2018-07-24 13:43:43 +0900

    これでどうや！

---
+++ src/main/java/com/example/baseball/HumanShopApplication.java
@@ -0,0 +1,12 @@
+package com.example.baseball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanShopApplication.class, args);
+	}
+}
